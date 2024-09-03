7988967dadb270c792644a8a021d7e4a18b9094b
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-09-03 19:35:53 +0500

    updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,8 +4,8 @@ import Records from "../components/Records";
 import Strengths from "../components/Strengths";
 export default function About(){
     return(
-      <div className="min-w-svw min-h-svh">
-         <div className="w-full h-svh bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
+      <div className="min-w-svw min-h-svh bg-slate-900">
+         <div className="w-full h-96 bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
         <h1 data-aos="fade-down" data-aos-duration="600" className="font-bold md:text-5xl text-3xl mb-3 text-center">About Us</h1>
         <div className="flex justify-center">
         <Breadcrumbs/>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,7 +5,7 @@ import OurMap from "../components/OurMap";
 export default function About(){
     return(
       <div className="min-w-svw min-h-svh bg-slate-900">
-       <div className="w-full h-svh bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
+       <div className="w-full h-96 bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
         <h1 data-aos="fade-down"  className="font-bold md:text-5xl text-3xl mb-3 text-center">Contact Us</h1>
         <div className="flex justify-center">
 <Breadcrumbs/>
