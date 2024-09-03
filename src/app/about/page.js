8ec013f6686efea6d8b6620e8af9a541aff3826d
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-09-03 21:47:00 +0500

    updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,7 +6,7 @@ import Strengths from "../components/Strengths";
 export default function About() {
   return (
     <div className="min-w-svw min-h-svh bg-slate-900">
-      <div className="w-full h-svh bg-slate-900 bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
+      <div className="w-full size-96 bg-slate-900 bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
         <h1 data-aos="fade-down" data-aos-duration="600" className="font-bold md:text-5xl text-3xl mb-3 text-center text-white">
           About Us
         </h1>
