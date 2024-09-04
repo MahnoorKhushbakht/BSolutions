56bc1c5169a7cb2544d4fcf348cf5c1faa8c70f7
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-09-04 22:14:04 +0500

    updated

--- src/app/components/SolutionLayout.js
+++ src/app/components/SolutionLayout.js
@@ -8,7 +8,7 @@ export default function SolutionLayout({children}) {
         <h1 data-aos="fade-down" className="text-white font-extrabold text-2xl md:text-3xl">Our Services and Solutions</h1>
         <p data-aos="fade-down" className="text-white font-semibold text-lg md:text-2xl text-center">Your IT Infrastructure is Entrusted to Us</p>
         {pathname !== '/services' && (
-        <div className='flex justify-center md:mt-5 mt-10 mb-0 md:mb-10'>
+        <div className='flex justify-center md:mt-5 mt-3 mb-0 md:mb-10'>
           <ServiceBtn>
             All Services
           </ServiceBtn>
