36c6a5dfe293d160914a24baa782ad705a702a09
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-09-07 01:39:29 +0500

    updated

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -7,7 +7,7 @@ export default function Services(){
     return(
         <div className="h-auto w-full ">
         <div className="w-full  h-80 md:h-[90vh] bg-[url('/images/page-header.jpg')] bg-cover bg-center grid place-items-center place-content-center">
-        <h1 data-aos="fade-down" data-aos-duration="600" className="font-bold md:text-5xl text-3xl mb-3 text-center">Services</h1>
+        <h1 data-aos="fade-down" data-aos-duration="600" className="font-bold md:text-5xl text-white text-3xl mb-3 text-center">Services</h1>
         <div className="flex justify-center">
         <Breadcrumbs/>
       </div>
